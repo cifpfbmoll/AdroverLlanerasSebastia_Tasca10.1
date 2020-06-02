4a4eca8f5d3fb7d8de6e101e2f2bec831331b7ba
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-06-03 00:19:54 +0200

    refactor: Arreglar tamaño columnas tabla

--- logica.js
+++ logica.js
@@ -16,28 +16,31 @@ function cargarXML(xml){
     var tabla = "";
     var noticias = docXML.getElementsByTagName("item");
     for (var i = 0; i < noticias.length; i++){
-        tabla += "<tr><td>";
+        tabla += "<tr><td style='width: 30%;'>";
         tabla += noticias[i].getElementsByTagName("title")[0].textContent;
         tabla += "</td>";
             if (typeof noticias[i].getElementsByTagName("media:content")[0] != "undefined") {
                 tabla += filtrar(noticias[i].getElementsByTagName("media:content")[0].getAttribute("type"), noticias[i].getElementsByTagName("media:content")[0].getAttribute("url"))
+            } else {
+                tabla += "<td><img width='300' height='220' src='img/coollogo_com-23003894.png' alt='logo'></td><td>";
             }
         tabla += noticias[i].getElementsByTagName("pubDate")[0].textContent;
-        tabla += "</td><td>";
+        tabla += "</td><td><a class='btn btn-primary' href='";
         tabla += noticias[i].getElementsByTagName("link")[0].textContent;
-        tabla += "</td></tr>";
+        tabla += "'>Link</a></td></tr>";
     }
     document.getElementById("tb").innerHTML = tabla;
 
 }
-
 function filtrar(typeImage, itemMedia){
-    var contenido = "";
-    if (typeImage.substr(0, 9) === "image") {
-        contenido += "<td><img src='" + itemMedia + "'></td><td>";
-    } else {
-        contenido += '<td><video width="320" height="240" controls ><source src="'
+    var media = "";
+
+    console.log(typeImage.substr(0, 5))
+    if (typeImage.substr(0, 5) === "image") {
+        media += "<td><img width='300' height='220' src='" + itemMedia + "'></td><td>";
+    } else if (typeImage.substr(0, 5) === "video"){
+        media += '<td><video width="300" height="220" controls ><source src="'
             + itemMedia + '" type="' + typeImage + '">Video</video></td><td>';
     }
-    return contenido;
+    return media;
 }
